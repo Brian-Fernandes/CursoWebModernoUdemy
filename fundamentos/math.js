489d5bd8b82ffb31141a4be799eee82281a35bd1
Brian Fernandes <brian12fernandes@gmail.com>
2020-03-04 17:30:03 -0300

    Comite de Math.js, numeros.js e tipagemFraca.js

---
+++ fundamentos/math.js
@@ -0,0 +1,15 @@
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2)
+
+console.log(area.toFixed(2))
+
+// Retorna a raiz quadrada
+console.log(Math.sqrt(81))
+//Retorna o valor arrendodado de x, para o valor inteiro mais próximo.
+console.log(Math.round(0.9))
+//Retorna a base x elevada à potência y do expoente, ou seja, xy.
+console.log(Math.pow(2,2))
+
+//Retorna se o número é positivo(1), negativo(-1) ou zero(0)
+const numero = -0
+console.log(Math.sign(numero))
---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,10 @@
+let qualquer = 'Legal' //String texto
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.156 // number
+
+// Evitar nome genéricos e siglas
+let valor = ''
+let numero = 1
+let pqp = false 
