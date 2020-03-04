68356da2608a6e1529acd95b35eac45c40563f80
Brian Fernandes <brian12fernandes@gmail.com>
2020-03-04 17:56:44 -0300

    Comitando até a aula 22

---
+++ fundamentos/strings.js
@@ -0,0 +1,17 @@
+const escola = 'Cod3r'
+/*  
+    Devolve a letra que está no indice indicado(x)
+    começando pelo indice 0
+    Se o indice não for "Achado" retornará vazio
+*/
+console.log(escola.charAt(4))
+
+/* 
+    * Retorna se contém este indice na operação
+*/
+console.log(escola.indexOf('3'))
+
+/*
+    *Retorna um array com os valores separando-os por uma vírgula
+*/
+console.log('Ana,Maria,Pedro'.split(','))
---
+++ fundamentos/templateStrings.js
@@ -0,0 +1,10 @@
+const nome = 'Rebeca'
+const concatenacao = 'Olá' + nome + '!'
+const template = `Olá ${nome}!`
+
+
+//expressoes...
+console.log(`1 + 1 = ${1+1}`)
+
+const up = texto => texto.toUpperCase() //arrow function
+console.log(`Ei... ${up('cuidado')} !`)
