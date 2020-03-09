098deb27de48686fd5dadc1ee707d347a35e391a
Brian Fernandes <brian12fernandes@gmail.com>
2020-03-09 16:38:49 -0300

    Aulas visualizadas 09/03

---
+++ fundamentos/objeto.js
@@ -0,0 +1,12 @@
+const produto1 = {}
+
+produto1.nome = 'Celular Ultra Mega'
+produto1.preco = 4998.90
+produto1['Desconto Legal'] = 0.40 // Evitar atributos com espaços 
+
+console.log(produto1)
+
+const produto2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90
+}
