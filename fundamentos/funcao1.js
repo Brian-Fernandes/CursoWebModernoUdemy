6430d6e564f2296886c2702a8c88ea4d215cd857
Brian Fernandes <brian12fernandes@gmail.com>
2020-03-09 17:59:43 -0300

    Até aula 27

---
+++ fundamentos/funcao1.js
@@ -0,0 +1,16 @@
+// Função sem retorno
+
+function imprimirSoma(a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+
+// Função com retorno
+
+function soma(a, b = 0) { //atributo b está como default 0
+    return a + b
+}
+
+console.log(soma( 2, 3)) // 5 
+console.log(soma(2)) // 2 pois o atributo b pega o default
